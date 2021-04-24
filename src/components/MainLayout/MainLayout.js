d90a2090fcddde6571a89d135be82ee2c43dbb1a
Petre-Marian Vaduva <rik_296@yahoo.com>
2021-04-24 13:02:30 +0100

    add navbar and main-layout components

---
+++ src/components/MainLayout/MainLayout.js
@@ -0,0 +1,9 @@
+import './MainLayout.css'
+
+function MainLayout() {
+    return (
+        <div className='MainLayout'>This is MainLayout</div>
+    );
+}
+
+export default MainLayout;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,9 @@
+import './Navbar.css'
+
+function Navbar() {
+    return (
+        <div className='navbar'>This is Navbar</div>
+    );
+}
+
+export default Navbar;
